01f20f666a6ed54e3ab42a8fa1371d703444ac69
Abdul Rafay <abdulrafay23butt@gmail.com>
2024-05-09 12:39:48 +0500

    login signup

--- src/login.jsx
+++ src/login.jsx
@@ -1,52 +1,49 @@
-// App.jsx
 import React, { useState } from 'react';
+import Signup from './Signup';
 
-
-function Login() {
+const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   const handleToggle = () => {
-    setIsLogin(prevState => !prevState);
+    setIsLogin(!isLogin);
   };
 
   return (
-    <div className="container">
-      <div className="two-thirds">
-        <div className="form-container">
-          <h2>Login to your account</h2>
-          <form>
-            <div className="form-field">
-              <label htmlFor="username">Username:</label>
-              <input type="text" id="username" />
-            </div>
-            <div className="form-field">
-              <label htmlFor="password">Password:</label>
-              <input type="password" id="password" />
-            </div>
-            <button type="submit" className="blue-button">Sign in</button>
-          </form>
-        </div>
-      </div>
-      <div className="one-third">
-        <div className="content">
-          {isLogin ? (
-            <>
-              <h2>New Here</h2>
-              <p>Sign Up to discover new job Opportunities</p>
-              <div className="button-container">
-                <button onClick={handleToggle}>Sign up</button>
+    <div>
+      <form action='/' method='post'>
+        {isLogin ? (
+          <div className="container">
+            <div className="two-thirds">
+              <div className="form-container">
+                <h2>Login to your account</h2>
+                <div className="form-field">
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" name='user' />
+                </div>
+                <div className="form-field">
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name='pass' />
+                </div>
+                <button type="submit" className="blue-button">Sign in</button>
               </div>
-            </>
-          ) : (
-            <>
-              <h2>Welcome Back!</h2>
-              <p>Login to discover new job Opportunities</p>
-            </>
-          )}
-        </div>
-      </div>
+            </div>
+            <div className="one-third">
+              <div className="content">
+                <h2>New Here</h2>
+                <p>Sign Up to discover new job Opportunities</p>
+                <div className="button-container">
+                  <button onClick={handleToggle}>Sign up</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Signup/>
+        )}
+      </form>
     </div>
   );
-}
+  
+};
 
 export default Login;
