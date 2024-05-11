132c89aa709b4a220ef5b4c662832ef3a75ea82e
Muhammad Islam <miso60800@gmail.com>
2024-05-11 21:42:33 +0500

    changes in styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Homepage from "./Homepage";
 function App() {
   return (
     <div>
       {/* <Login />
       <Signup /> */}
-      {/* <Homepage /> */}
-       <About/>
+      <Homepage />
+       {/* <About/> */}
      {/* <Createposte/> */}
     </div>
   );
