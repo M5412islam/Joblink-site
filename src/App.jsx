f95b5d8e81ec29e3bbcaf5fb42c439807dfaaa5e
Abdul Rafay <abdulrafay23butt@gmail.com>
2024-05-11 21:18:59 +0500

    Signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Homepage from "./Homepage";
+// import Homepage from "./Homepage";
+ import Signup from "./Signup";
+import Login from "./login";
 function App() {
   return (
     <div>
-      {/* <Login />
-      <Signup /> */}
-      <Homepage />
+       {/* <Login /> */}
+      <Signup />
+      {/* <Homepage /> */}
     </div>
   );
 }
