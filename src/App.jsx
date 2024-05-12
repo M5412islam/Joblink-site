6a3437fe0eb63d22b36603450a68280cd30912c9
abdulrafay23butt <102746193+abdulrafay23butt@users.noreply.github.com>
2024-05-12 21:52:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-
-// import About from "./About";
-// import CreatePost from "./Create-Post";
-// import Homepage from "./Homepage";
-// import Signup from "./Signup";
-import Login from "./login";
-
+import React,{ useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CPosts from "./Create-Post";
+import Homepage from "./Homepage";
+import About from "./About";
+ import Signup from "./Signup";
+ import Login from "./login";
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
   return (
-    <div>
-      <Login />
-      {/* <Signup /> */}
-      {/* <Homepage /> */}
-       {/* <About/> */}
-     {/* <CreatePost/> */}
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route index element={<Homepage setIsLogged={isLogged}/>} />
+          <Route path="Create_Post" element={<CPosts/>} />
+          <Route path="About" element={<About/>}/>
+          <Route path="Signup" element={<Signup/>}/>
+          <Route path="Login" element={<Login setIsLogged={setIsLogged}/>}/>
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
