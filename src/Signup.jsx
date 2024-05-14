13c96bd1fc1a37f4f9d8f422f19929b4ba308640
Abdul Rafay <abdulrafay23butt@gmail.com>
2024-05-14 20:01:25 +0500

    updated signup login again

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import Login from "./login";
+import backgroundimage from "./Images-Logos/backgroundimage.jpeg";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
     first: '',
     last: '',
-    Username: '',
-    Email: '',
-    Password: ''
+    username: '', // corrected to lowercase
+    email: '', // corrected to lowercase
+    password: '' // corrected to lowercase
   });
 
+  const [isSignup, setIsSignup] = useState(true);
   const [message, setMessage] = useState('');
 
   const handleChange = (event) => {
@@ -30,57 +33,66 @@ const Signup = () => {
         body: JSON.stringify(formData)
       });
       const data = await response.json();
-      if(data.message === "User created successfully") {
-        window.location.href = '/login'; 
+      if (data.message === "User created successfully") {
+        window.location.href = '/login';
       } else {
         setMessage(data.message);
       }
     } catch (error) {
-      console.error('Error logging in:', error);
+      console.error('Error signing up:', error);
+      setMessage('An error occurred. Please try again later.');
     }
   };
 
+  const handleToggle = () => {
+    setIsSignup(!isSignup);
+  };
+
   return (
-    <div className="container">
-      <div className="two-thirds">
-        <div className="form-container">
-        <p className='message'>{message}</p>
-          <h2>Create your account</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="form-field">
-              <label htmlFor="firstname">First Name:</label>
-              <input type="text" id="firstname" name='first' value={formData.first} onChange={handleChange} required/>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {isSignup ? (
+          <div className="container">
+            <div className="two-thirds">
+              <div className="form-container">
+                <p className='message'>{message}</p>
+                <h2 className='line'>Create your account</h2>
+                <div className="form-field">
+                  <label htmlFor="firstname">First Name:</label>
+                  <input type="text" id="firstname" name='first' value={formData.first} onChange={handleChange} required/>
+                </div>
+                <div className="form-field">
+                  <label htmlFor="lastname">Last Name:</label>
+                  <input type="text" id="lastname" name='last' value={formData.last} onChange={handleChange} required/>
+                </div>
+                <div className="form-field">
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" name='username' value={formData.username} onChange={handleChange} required/>
+                </div>
+                <div className="form-field">
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" name='email' value={formData.email} onChange={handleChange} required/>
+                </div>
+                <div className="form-field">
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name='password' value={formData.password} onChange={handleChange} required/>
+                </div>
+                <button type="submit" className="blue-button">Sign up</button>
+              </div>
             </div>
-            <div className="form-field">
-              <label htmlFor="lastname">Last Name:</label>
-              <input type="text" id="lastname" name='last' value={formData.last} onChange={handleChange} required/>
+            <div className='one-third' style={{ backgroundImage: `url(${backgroundimage})`, backgroundRepeat: 'no-repeat', color: 'black' }}>
+              <div className="content">
+                <h2>{'Already have an account?' }</h2>
+                <p>{'Get back to your profile' }</p>
+                <div className="button-container">
+                  <button  style={{backgroundColor:'black'}} onClick={handleToggle}>{isSignup ? 'Login' : 'Sign up'}</button>
+                </div>
+              </div>
             </div>
-            <div className="form-field">
-              <label htmlFor="username">Username:</label>
-              <input type="text" id="username" name='Username' value={formData.Username} onChange={handleChange} required/>
-            </div>
-            <div className="form-field">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name='Email' value={formData.Email} onChange={handleChange} required/>
-            </div>
-            <div className="form-field">
-              <label htmlFor="password">Password:</label>
-              <input type="password" id="password" name='Password' value={formData.Password} onChange={handleChange} required/>
-            </div>
-            <button type="submit" className="blue-button">Sign up</button>
-          </form>
-          {message && <p>{message}</p>}
-        </div>
-      </div>
-      <div className="one-third">
-        <div className="content">
-          <h2>Already have an account?</h2>
-          <p>Get back to your profile</p>
-          <div className="button-container">
-            <button>Login</button>
           </div>
-        </div>
-      </div>
+        ) : null}
+      </form>
+      {!isSignup && <Login/> }
     </div>
   );
 };
